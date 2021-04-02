179f2b2db9c476a479541b7cba623dd24fd5f3d7
NJL <58279268+njlgbrl@users.noreply.github.com>
2021-04-02 09:17:26 +0800

    Added the basic functionality

---
+++ main.py
@@ -0,0 +1,53 @@
+from configparser import ConfigParser
+import time
+
+from pypresence import Presence
+
+config_file = 'config.ini'
+config = ConfigParser()
+config.read(config_file)
+
+client_id = config['Client']['ClientID']
+
+if config['States']['State'] == '<Required>':
+    state = config['States']['State']
+else:
+    state = 'Left unchanged'
+
+if config['States']['Details'] != '<Optional>':
+    details = config['States']['Details']
+else:
+    details = None
+
+if config['States']['ElapsedTime'] == 'True':
+    start = time.time()
+else:
+    start = None
+
+if config['Images']['LargeImage'] != '<Optional>':
+    large_image = config['Images']['LargeImage']
+else:
+    large_image = None
+
+if config['Images']['SmallImage'] != '<Optional>':
+    small_image = config['Images']['SmallImage']
+else:
+    small_image = None
+
+if config['Images']['LargeTooltip'] != '<Optional>':
+    large_tooltip = config['Images']['LargeTooltip']
+else:
+    large_tooltip = None
+
+if config['Images']['SmallTooltip'] != '<Optional>':
+    small_tooltip = config['Images']['LargeTooltip']
+else:
+    small_tooltip = None
+
+rpc = Presence(client_id)
+rpc.connect()
+rpc.update(state=state, details=details, start=start, large_image=large_image,
+           small_image=small_image, large_text=large_tooltip, small_text=large_tooltip)
+
+while True:
+    time.sleep(15)
